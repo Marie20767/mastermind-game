3f2740f59dfb8f42631ddba7f6cddb6f7c36664b
Marie20767 <marie20767@gmail.com>
2022-12-13 11:42:35 +0000

    Fixed baseUrl issue in cypress config file

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,6 +1,8 @@
 describe('renders the app', () => {
   beforeEach(() => {
     cy.visit('/');
+    // using commands:
+    // cy.login();
   });
 
   it('app container renders correctly', () => {
@@ -15,5 +17,55 @@ describe('renders the app', () => {
 // cy.findByText("Boston Celtics").click();
 // cy.url().should("include", "teams/2");
 // cy.findByText("Confrerence: East").should("exist")
+
+// it('add todo', () => {
+// cy.findByRole('textbox', { name: 'title' })
+// .type('Todo1')
+// .type('{enter}')
+// cy.findByText("Todo1").should('exist')
+// })
+// it('remove todo', () => {
+// cy.findByRole('textbox', { name: /title/i })
+// .type('Todo1')
+// .type('{enter}')
+
+// cy.wait(2000) -> otherwise the remove happens too fast
+// cy.findByRole('button', {name: remove}).click()
+// cy.findByText('todo1').should('not exist')
+
+// If you have multiple buttons that are called the same you can use test-id called data-cy
+// then: cy.get('[data-cy=todo-Todo1]').within(() => {
+// cy.findByRole('button', {name: 'remove'}).click()
+// })
+// })
+
+// E2E Example:
+describe('todos', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('user can add, check and delete todos', () => {
+    cy.findByRole('textbox', { name: /title/i })
+      .type('Todo1')
+      .type('{enter}');
+
+    cy.findByRole('textbox', { name: /title/i })
+      .type('Todo2')
+      .type('{enter}');
+
+    cy.findByText('Todo1').should('exist');
+    cy.findByText('Todo2').should('exist');
+    cy.findByText('total todos: 3').should('exist');
+  });
+
+  cy.findByRole('checkbox', { name: 'todo2' }).check();
+  cy.findByText('selected todos: 1').should('exist');
+  cy.get('[data-cy=todo-Todo1]').within(() => {
+    cy.findByRole('button', { name: 'remove' }).click();
+  });
+
+  cy.findByText('total todos: 1').should('exist');
+});
 
 export {};
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,1 +1,7 @@
 import '@testing-library/cypress/add-commands';
+
+// If you have an app where you need to login first everytime you test the app you can add commands
+// Cypress.Commands.add('login', () => {
+//   cy.visit('/');
+//   cy.findByRole('button', { name: 'click here to login' }).click();
+// });
