f27fe334aad5163c28bd8988b44d1d52b163878b
Marie20767 <marie20767@gmail.com>
2022-12-14 12:25:05 +0000

    Wrote e2e tests for winning after 5 rounds and loosing the game

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,5 +1,5 @@
-import { allRoundPegsAreEmpty } from '../../src/tests/test-constants';
-import { checkColorOfAllArrowsGameStart, checkSolution, selectPegsDelete1RoundAndGetFeedback, checkOverlayAndStartNewGame } from './test-utils';
+import { allRoundPegsAreEmpty, CorrectFeedbackColor, WrongPositionFeedbackColor, WrongFeedbackColor } from '../../src/tests/test-constants';
+import { checkColorOfAllArrows, checkSolution, selectPegsDelete1RoundAndGetFeedback, checkOverlayAndStartNewGame, playRoundsAndGetFeedback } from './test-utils';
 
 const solution = ['#f7d840', '#06ba7e', '#2b9de5', '#2b9de5'];
 
@@ -10,24 +10,34 @@ describe('play 1 full game', () => {
 
   it('wins on the first round', () => {
     // check the setup before playing (arrow showing correctly, solution hidden)
-    checkColorOfAllArrowsGameStart();
+    checkColorOfAllArrows(0);
     checkSolution(allRoundPegsAreEmpty);
     selectPegsDelete1RoundAndGetFeedback(['#fff', '#ac274d', '#fff', '#ac274d'], solution);
     checkSolution(solution);
-    checkOverlayAndStartNewGame();
+    checkOverlayAndStartNewGame('Congratulations!', 'Games won: 1', 'Games lost: 0');
   });
 
-  // it('wins after 5 rounds', () => {
+  it('wins after 5 rounds', () => {
+    const selectedColorsNotMatchingSolution = ['#f7d840', '#fff', '#06ba7e', '#2b9de5'];
+    const feedbackColors1 = [CorrectFeedbackColor, CorrectFeedbackColor, WrongPositionFeedbackColor, WrongFeedbackColor];
+    const selectedColorsMatchingSolution = solution;
+    const feedbackColors2 = [CorrectFeedbackColor, CorrectFeedbackColor, CorrectFeedbackColor, CorrectFeedbackColor];
 
-  // });
+    playRoundsAndGetFeedback(4, selectedColorsNotMatchingSolution, feedbackColors1, selectedColorsMatchingSolution, feedbackColors2);
+    checkSolution(solution);
+    checkOverlayAndStartNewGame('Congratulations!', 'Games won: 1', 'Games lost: 0');
+  });
 
-  // it('looses the game', () => {
+  it.only('looses the game', () => {
+    const selectedColorsNotMatchinSolution1 = ['#fff', '#fff', '#06ba7e', '#ac274d'];
+    const feedbackColors1 = [WrongPositionFeedbackColor, WrongFeedbackColor, WrongFeedbackColor, WrongFeedbackColor];
+    const selectedColorsNotmatchingSolution2 = ['#06ba7e', '#f7d840', '#f7d840', '#f7d840'];
+    const feedbackColors2 = [WrongPositionFeedbackColor, WrongPositionFeedbackColor, WrongFeedbackColor, WrongFeedbackColor];
 
-  // });
+    playRoundsAndGetFeedback(8, selectedColorsNotMatchinSolution1, feedbackColors1, selectedColorsNotmatchingSolution2, feedbackColors2);
+    checkSolution(solution);
+    checkOverlayAndStartNewGame('You lost...', 'Games won: 0', 'Games lost: 1');
+  });
 });
-
-// Examples:
-// cy.get(.date).type("2021-02-17")
-// cy.wait(2000) -> otherwise the remove happens too fast
 
 export {};
--- cypress/e2e/test-utils.js
+++ cypress/e2e/test-utils.js
@@ -1,13 +1,11 @@
 import { allRoundPegsAreEmpty, CorrectFeedbackColor } from '../../src/tests/test-constants';
 
-export const checkColorOfAllArrowsGameStart = () => {
-  cy.get('[data-testid="round-0"]').within(() => {
-    cy.get('svg').should('have.attr', 'color', '#4e4e4c');
-  });
+export const checkColorOfAllArrows = (roundIndex) => {
+  for (let i = 0; i < 8; i++) {
+    const expectedArrowColor = i === roundIndex ? '#4e4e4c' : 'transparent';
 
-  for (let i = 1; i < 8; i++) {
     cy.get(`[data-testid=round-${i}]`).within(() => {
-      cy.get('svg').should('have.attr', 'color', 'transparent');
+      cy.get('svg').should('have.attr', 'color', expectedArrowColor);
     });
   }
 };
@@ -58,21 +56,50 @@ export const selectPegsDelete1RoundAndGetFeedback = (selectedColorsNotMatchingSo
   });
 };
 
-export const checkOverlayAndStartNewGame = () => {
+export const playRoundsAndGetFeedback = (rounds, selectedColors1, feedbackColors1, selectedColors2, feedbackColors2) => {
+  for (let i = 0; i < rounds; i++) {
+    checkColorOfAllArrows(i);
+    for (let j = 0; j < selectedColors1.length; j++) {
+      cy.get(`[data-testid="pegpicker-${selectedColors1[j]}"]`).click();
+    }
+
+    cy.findByRole('button', { name: 'Check' }).click();
+    cy.get(`[data-testid="round-${i}"]`).within(() => {
+      for (let k = 0; k < feedbackColors1.length; k++) {
+        cy.get(`[data-testid="pegfeedback-${k}-${feedbackColors1[k]}"]`).should('exist');
+      }
+    });
+  }
+
+  for (let i = 0; i < selectedColors2.length; i++) {
+    cy.get(`[data-testid="pegpicker-${selectedColors2[i]}"]`).click();
+  }
+
+  cy.findByRole('button', { name: 'Check' }).click();
+  for (let i = 0; i < feedbackColors2.length; i++) {
+    cy.get(`[data-testid="pegfeedback-${i}-${feedbackColors2[i]}"]`).should('exist');
+  }
+};
+
+const checkGameIsSetupForNewGame = () => {
+  // Check solution is hidden
+  checkSolution(allRoundPegsAreEmpty);
+  // Check arrow is showing at the bottom
+  checkColorOfAllArrows(0);
+  // Check that the round pegs are all empty and no feedback is showing
+  checkTheGameBoardIsEmptyOfPegs();
+};
+
+export const checkOverlayAndStartNewGame = (title, gamesWonScore, gamesLostScore) => {
   // Check correct overlay is showing and click on 'New Game'
-  cy.findByText('Congratulations!').should('exist');
+  cy.findByText(title).should('exist');
   cy.findByRole('button', { name: 'New Game' }).click();
 
   // Check scores
   cy.get('[class*="desktop-score"]').within(() => {
-    cy.findByText('Games won: 1').should('exist');
-    cy.findByText('Games lost: 0').should('exist');
+    cy.findByText(gamesWonScore).should('exist');
+    cy.findByText(gamesLostScore).should('exist');
   });
 
-  // Check solution is hidden
-  checkSolution(allRoundPegsAreEmpty);
-  // Check arrow is showing at the bottom
-  checkColorOfAllArrowsGameStart();
-  // Check that the round pegs are all empty and no feedback is showing
-  checkTheGameBoardIsEmptyOfPegs();
+  checkGameIsSetupForNewGame();
 };
